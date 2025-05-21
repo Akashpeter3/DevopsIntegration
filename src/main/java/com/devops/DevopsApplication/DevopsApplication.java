8da858d820dc458ae18de090c5e3df3623958d30
Akash K K <Akash.KK@ibm.com>
2025-05-21 16:46:22 +0530

    Added changes

--- src/main/java/com/devops/DevopsApplication/DevopsApplication.java
+++ src/main/java/com/devops/DevopsApplication/DevopsApplication.java
@@ -3,12 +3,14 @@ package com.devops.DevopsApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
 public class DevopsApplication {
 
 
 	public static void main(String[] args) {
-		System.out.println(" Devops Application is starting.....................................");
+		System.out.println(" Devops Application is starting on date "+new Date());
 		SpringApplication.run(DevopsApplication.class, args);
 	}
 
