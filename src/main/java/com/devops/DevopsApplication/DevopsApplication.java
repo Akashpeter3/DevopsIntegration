069db3c44f8878db8548a16240ec16f0bd10a940
Akash K K <Akash.KK@ibm.com>
2025-05-21 16:17:36 +0530

    Added changes

--- src/main/java/com/devops/DevopsApplication/DevopsApplication.java
+++ src/main/java/com/devops/DevopsApplication/DevopsApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DevopsApplication {
 
+
 	public static void main(String[] args) {
+		System.out.println(" Devops Application is starting.....................................");
 		SpringApplication.run(DevopsApplication.class, args);
 	}
 
